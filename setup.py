a70386d92d8328255575726b526b4f34c5d8ce97
holy-tanuki <67459553+holy-tanuki@users.noreply.github.com>
2022-03-15 22:51:15 +0100

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='wombopy',
-    version='0.1.1',
+    version='0.1.2',
     description='A module to generate wombo.art images',
     author='Holy Tanuki',
     long_description=long_description,
