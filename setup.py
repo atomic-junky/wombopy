6cd17e9b43190596bd53ca14c7e8d07b99b8e83e
holy-tanuki <67459553+holy-tanuki@users.noreply.github.com>
2022-03-15 23:21:34 +0100

    Update setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name="wombopy",
     version="0.1.2",
     description="A module to generate wombo.art images",
     author="Holy Tanuki",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=["wombopy"],
     install_requires=[
         "typer==0.4.0",
         "colorama==0.4.4",
         "shellingham==1.4.0",
-        "pytest==7.0.1",
+        "pytest==7.1.0",
         "requests==2.27.1",
+        "Pillow==9.0.1",
+        "setuptools==60.9.3",
     ],
+    package_dir={"": "wombopy"},
+    packages=setuptools.find_packages(where="wombopy"),
     python_requires=">=3.9",
 )
