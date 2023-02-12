d0410bf3c57c98d0045a9b49a6535c69931cdc68
Holy Crusader <holy.tanukis@gmail.com>
2023-02-12 22:13:32 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "typer==0.4.1",
-        "colorama==0.4.4",
-        "shellingham==1.4.0",
-        "pytest==7.1.0",
-        "requests>=2.23.0"
+        "typer==0.7.0",
+        "colorama==0.4.6",
+        "shellingham==1.5.0post1",
+        "pytest==7.1.2",
+        "requests==2.28.2"
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
