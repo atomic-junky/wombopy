ac39135eb54ad4cb5c5206bed5273297f8545aec
Holy Crusader <holy.tanukis@gmail.com>
2023-06-01 17:57:49 +0200

    Update requirements, author and version

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wombopy",
-    version="0.2.2",
+    version="0.2.3",
     description="A module to generate wombo.art images",
-    author="Holy Tanuki",
+    author="AtomicDreamz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "typer==0.7.0",
+        "typer==0.9.0",
         "colorama==0.4.6",
         "shellingham==1.5.0post1",
-        "pytest==7.1.2",
-        "requests==2.28.2"
+        "pytest==7.3.1",
+        "requests==2.31.0"
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
